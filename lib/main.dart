84427a381c5c92c0cbd08d92286ad97ddbf462f0
Breno Barbosa <breno.barbosa03@souunit.com.br>
2026-05-20 19:43:16 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/onboarding.dart';
+import 'pages/agenda.dart';
 
 void main() {
   runApp(const EventPlannerApp());
@@ -12,7 +12,7 @@ class EventPlannerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Onboarding(),
+      home: Agenda(),
     );
   }
 }
